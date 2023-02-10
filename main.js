9a621bca7adb9e357fc70501eb3f417466720164
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-02-10 14:10:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//Exersice 1
+
+let a = 12;
+let b = 23;
+
+let p = (a + b) * 2;
+let r = a * b;
+alert(p);
+alert(r);
+
+//Exersice 2
+
+let c = 35;
+
+let u = c + 10;
+let d = c * 20;
+let h = c - 30;
+let l = c / 5;
+console.log(u)
+console.log(d)
+console.log(h)
+console.log(l)
